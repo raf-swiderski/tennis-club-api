54419949f88ca1553359450c18dd220f3cd08d58
Raf Swiderski <rafswid96@gmail.com>
2022-03-07 14:11:38 +0000

    setup with mongodb, register players model and get & post route

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
 const app = express()
-const port = 3000
+const port = process.env.PORT
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Connected to database'))
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('static'))
 
+app.get('/', (req, res) => {
+    res.redirect('/register')
+})
+
+const Player = require('./models/player')
+
 app.route('/register')
-  .get((req, res) => {
-    res.sendFile('static/register.html', {root: __dirname })
-  })
-  .post((req, res) => {
-      console.log(req.body)
-  })
+  
+    .get((req, res) => {
+        res.sendFile('static/register.html', {root: __dirname })
+    })
+
+    .post( async (req, res) => {
+    
+        const player = new Player({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            nationality: req.body.nationality,
+            dob: req.body.dob
+        })
+    
+        try {
+            const newPlayer = await player.save()
+            res.status(201).json(newPlayer)
+        } catch (error) {
+            res.status(400).json({ message: error.message })
+        }   
+
+    })
+/
+
+// app.use(express.json())
+
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
---
+++ models/player.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const playerSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    nationality: {
+        type: String,
+        required: true
+    },
+    dob: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Player', playerSchema)
