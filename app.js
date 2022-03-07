23afd9028b7d296851a373a1dc7c0dd75212b858
Raf Swiderski <rafswid96@gmail.com>
2022-03-07 10:48:14 +0000

    register route initialised

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
-// app.use(express.static('static'))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('static'))
 
-app.get('/', (req, res) => { 
-    res.send('tennis')
-})
+app.route('/register')
+  .get((req, res) => {
+    res.sendFile('static/register.html', {root: __dirname })
+  })
+  .post((req, res) => {
+      console.log(req.body)
+  })
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
