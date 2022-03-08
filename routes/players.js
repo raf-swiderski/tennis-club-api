8c93c4f5c53c8ee03033822960ee272e85a455c0
Raf Swiderski <rafswid96@gmail.com>
2022-03-08 16:18:46 +0000

    minimum age to sign up complete, getAge middleware

--- routes/players.js
+++ routes/players.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 async function checkIfPlayerExists(req, res, next) {
     let player
-    try {
+    try { // checking the database
         player = await Player.find({ firstName: req.body.firstName, lastName: req.body.lastName }).exec();
         if (player == null) {
             return res.status(404).json({ message: 'Cannot find player'})
@@ -13,8 +13,14 @@ async function checkIfPlayerExists(req, res, next) {
     } catch (error) {
         return res.status(500).json({ message: error.message })
     }
-    res.player = player
+    res.player = player 
     next()
+}
+
+function getAge(birthDate) { // format: '1996-06-26' => 25
+
+    const yearInMs = 3.15576e+10 // This constant is one year in milliseconds
+    return Math.floor((new Date() - new Date(birthDate).getTime()) / yearInMs)
 }
 
 router.get('/', (req, res) => {
@@ -27,9 +33,17 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', checkIfPlayerExists, async (req, res) => {
-    
-    if (res.player[0] != null ) {
-        res.status(400).json({ message: 'this player already exists'})
+
+    const age = getAge(req.body.dob)
+    console.log(age)
+
+    if (age < 16 ) {
+
+        res.status(400).json({ message: 'Players must be at least 16 years old to be able to enter the club'})
+
+    } else if (res.player[0] != null ) {
+
+        res.status(400).json({ message: 'This player already exists'})
 
     } else {
         
